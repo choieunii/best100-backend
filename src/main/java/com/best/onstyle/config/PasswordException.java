01fcb3b5a102d15f66b225b2173cc1d9daa84f6d
choieunii <imej221@gmail.com>
2022-06-13 10:50:54 +0900

    chore : password 불일치 커스텀 Exception

---
+++ src/main/java/com/best/onstyle/config/PasswordException.java
@@ -0,0 +1,8 @@
+package com.best.onstyle.config;
+
+public class PasswordException extends RuntimeException {
+    private static final String MESSAGE = "비밀번호가 일치하지 않습니다.";
+    public PasswordException () {
+        super(MESSAGE);
+    }
+}
