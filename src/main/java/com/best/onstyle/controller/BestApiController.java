9ee13049f7dcd9511258aff62cde9a7ae845d764
choieunii <imej221@gmail.com>
2022-06-13 10:40:57 +0900

    feat : DB 내 TOP 100 조회 기능, 정렬 기준 - 기본 최신순, 추가 정렬 - 댓글 수 및 좋아요 수

---
+++ src/main/java/com/best/onstyle/controller/BestApiController.java
@@ -0,0 +1,24 @@
+package com.best.onstyle.controller;
+
+import com.best.onstyle.domain.best.Best;
+import com.best.onstyle.dto.BestItemRankingsResponseDto;
+
+import com.best.onstyle.dto.BestTop100ResponseDto;
+import com.best.onstyle.service.BestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BestApiController {
+
+    private final BestService bestService;
+
+    @GetMapping("/best/top100")
+    public List<BestTop100ResponseDto> findBest(@RequestParam(required = false) String sort) {
+        return bestService.findBestTop100(sort);
+    }
+
+}
---
+++ src/main/java/com/best/onstyle/service/BestService.java
@@ -0,0 +1,60 @@
+package com.best.onstyle.service;
+
+import com.best.onstyle.domain.best.Best;
+import com.best.onstyle.domain.best.BestRepository;
+import com.best.onstyle.domain.itemInfo.ItemInfo;
+import com.best.onstyle.domain.itemInfo.ItemInfoRepository;
+import com.best.onstyle.domain.reply.Reply;
+import com.best.onstyle.domain.reply.ReplyRepository;
+import com.best.onstyle.dto.BestItemRankingsResponseDto;
+import com.best.onstyle.dto.BestTop100ResponseDto;
+import com.best.onstyle.dto.ReplyResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BestService {
+    private final ItemInfoRepository itemInfoRepository;
+    private final BestRepository bestRepository;
+    private final ReplyRepository replyRepository;
+    @Transactional
+    public List<BestTop100ResponseDto> findBestTop100(String sortType) {
+        Date before = new Date();
+        before = new Date(before.getTime() + (1000 * 60 * 60 * 24 * -1));
+
+        Sort sort;
+        if(sortType == null){
+            sort = Sort.by(Sort.Direction.ASC, "ranking");
+        }else {
+            sort = getSortType(sortType);
+        }
+
+        List<Best> bestTop100 = bestRepository.findAllByCurrentUpdateBetween(before, new Date(), sort);
+
+        return bestTop100.stream().map(best -> {
+            ReplyResponseDto likeTopReply = replyRepository.findTopByItemInfoOrderByLikeCntDesc(best)
+                    .map(ReplyResponseDto::new).get();
+            ReplyResponseDto hateTopReply = replyRepository.findTopByItemInfoOrderByHateCntDesc(best)
+                    .map(ReplyResponseDto::new).get();
+            BestTop100ResponseDto bestTop100ResponseDto = new BestTop100ResponseDto(best, likeTopReply, hateTopReply);
+            return bestTop100ResponseDto;
+        }).collect(Collectors.toList());
+    }
+
+    private Sort getSortType(String sortType){
+        if(sortType.equals("replyCnt")){
+            return Sort.by(Sort.Direction.DESC, "countOfReplies");
+        }else if(sortType.equals("likeCnt")){
+            return Sort.by(Sort.Direction.DESC, "countOfItemLikes");
+        }
+        return Sort.by(Sort.Direction.DESC, "currentUpdate");
+    }
+
+}
